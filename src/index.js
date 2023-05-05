def140fcf8ae721cd5e51736b3a77cc1a8f956f0
Victoria Shmidt <reachme@victoriashmidt.com>
2023-05-05 13:47:31 -0600

    Delated default files in src

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const form = document.querySelector('.search-form');
 
+const BASE_URL = ghjfk;
 form.addEventListener('submit', event => {
   event.preventDefault();
   const input = form.elements.searchQuery.value;
