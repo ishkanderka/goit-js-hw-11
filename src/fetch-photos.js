53bfdd3b799b7fb865f70d5bdb7447c8ce665198
Victoria Shmidt <reachme@victoriashmidt.com>
2023-05-07 01:46:27 -0600

    Update fetch-photos.js

--- src/fetch-photos.js
+++ src/fetch-photos.js
@@ -2,19 +2,19 @@ import axios from 'axios';
 
 const BASE_URL = 'https://pixabay.com/api/';
 
-export async function fetchPhotos(input) {
-  const params = {
+export async function fetchPhotos(input, page = 1) {
+  const params = new URLSearchParams({
     key: '36072059-95eba457d7f0e9185349988ad',
     q: input,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
-    page: 1,
+    page: page,
     per_page: 40,
-  };
+  });
   try {
-    const { response } = await axios.get(BASE_URL, { params });
-    return response;
+    const { data } = await axios.get(BASE_URL, { params });
+    return data;
   } catch (error) {
     console.log(error.message);
   }
